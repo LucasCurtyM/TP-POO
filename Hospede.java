65650a4d168fc3c193a862009592420400aa44de
Tavares27 <felipe.t.sousa@ufv.br>
2024-12-17 20:13:34 -0300

    Primeira classe
    Criação da classe Hospede

---
+++ Hospede.java
@@ -0,0 +1,45 @@
+public class Hospede {
+	private String nome;
+    private String cpf;
+    private String email;
+    private boolean logado;
+
+    public Hospede(String nome, String cpf, String email, boolean logado) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.logado = logado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isLogado() {
+        return logado;
+    }
+
+    public void setLogado(boolean logado) {
+        this.logado = logado;
+    }
+}
